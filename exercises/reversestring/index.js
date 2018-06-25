2227ed652a41fcd8cdf2cfe65dbe0b14405ad456
Julio Graham <julioagraham@gmail.com>
2018-06-24 23:48:01 -0400

    Use debugger and REPL to inspect and debug code

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,16 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+//    debugger;
     return str.split('').reverse().join('');
 }
 
+reverse('Hello');
+
 module.exports = reverse;
+
+// let reversed = '';
+// for (let character of str) {
+//     reversed = character + reversed;
+// }
+// return reversed;
