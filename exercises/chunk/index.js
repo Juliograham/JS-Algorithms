c6c68aa14b18ed2155be2d4b9880746667262e83
Julio Graham <julioagraham@gmail.com>
2018-12-28 20:12:56 -0600

    Refactored solution for chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,20 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
+function chunk (array, size) {
     const holder = [];
-    
-    for (let element of array) {
-        const last = holder[holder.length - 1];
-
-        if (!last || last.length === size) {
-            holder.push([element]);
-        } else {
-            last.push(element);
-        }
+    let index = 0;
+    while (index < array.length) {
+        holder.push(array.slice(index, index + size));
+        index += size;
     }
 
     return holder;
 }
+
+
+
+
+
+// function chunk(array, size) {
+//     const holder = [];
+    
+//     for (let element of array) {
+//         const last = holder[holder.length - 1];
+
+//         if (!last || last.length === size) {
+//             holder.push([element]);
+//         } else {
+//             last.push(element);
+//         }
+//     }
+
+//     return holder;
+// }
 
 module.exports = chunk;
