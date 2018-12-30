bbce695f73835b795f1a44670b28016e5e045eee
Julio Graham <julioagraham@gmail.com>
2018-12-30 10:09:23 -0600

    Started anagram exercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,9 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const firstWord = stringA.replace(/[^\w]/g, '').toLowerCase();
+    console.log(firstWord);
+}
 
 module.exports = anagrams;
