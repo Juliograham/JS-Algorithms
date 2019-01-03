7e199e7af4d7b86be3e8d4304ce017820329f5a1
Julio Graham <julioagraham@gmail.com>
2019-01-02 23:26:24 -0600

    Refactor anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,30 +9,11 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-    const firstMap = buildCharMap(stringA);
-    const secondMap = buildCharMap(stringB);
-    
-    if (Object.keys(firstMap).length !== Object.keys(secondMap).length) {
-        return false;
-    }
-
-    for (let char in firstMap) {
-        if (firstMap[char] !== secondMap[char]) {
-            return false;
-        }
-    }
-
-    return true;
+    return cleanString(stringA) === cleanString(stringB);
 }
 
-const buildCharMap = (str) => {
-    const charMap = {};
-
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-        charMap[char] = charMap[char] + 1 || 1;
-    }
-
-    return charMap;
+const cleanString = (str) => {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
