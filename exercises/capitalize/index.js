aea0f1023916baae681fc9171c5f6bd67199692d
Julio Graham <julioagraham@gmail.com>
2019-01-02 23:41:01 -0600

    Completed capitalize exercise

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,14 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const answer = [];
+
+    for (word of str.split(' ')) {
+        answer.push(word[0].toUpperCase() + word.slice(1))
+    }
+
+    return answer.join(' ');
+}
 
 module.exports = capitalize;
