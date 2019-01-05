731dead140522868e8cd20d77590dc651c20bcb2
Julio Graham <julioagraham@gmail.com>
2019-01-05 17:13:37 -0600

    Solve vowels exercise with iterative and regular expression solutions

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+const vowels = (str) => {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+
+
+// function vowels(str) {
+//     const holder = ['a', 'e', 'i', 'o', 'u'];
+//     let answer = 0;
+
+//     for (char of str.toLowerCase()) {
+//         if (holder.includes(char)) {
+//             answer++;
+//         }
+//     }
+
+//     return answer;
+// }
 
 module.exports = vowels;
