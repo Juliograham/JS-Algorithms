9373e2ecc164be9c7b15345634edb3c5c27f10e3
Julio Graham <julioagraham@gmail.com>
2019-01-19 12:15:39 -0600

    Solve binary search tree exercise

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,24 +11,42 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {
-    const bookmark = 'done';
-    const counter = [0];
-    const arr = [root, bookmark];
+// function levelWidth(root) {
+//     const bookmark = 'done';
+//     const counter = [0];
+//     const arr = [root, bookmark];
 
-    while (arr.length > 1) {
-        const node = arr.shift();
+//     while (arr.length > 1) {
+//         const node = arr.shift();
 
+//         if (node === bookmark) {
+//             counter.push(0);
+//             arr.push(bookmark);
+//         } else {
+//             arr.push(...node.children);
+//             counter[counter.length - 1]++;
+//         }                
+//     }
+
+//     return counter;
+// }
+
+const levelWidth = (root) => {
+    const counters = [0];
+    const bookmark = 'Empty';
+    const holder = [root, bookmark];
+
+    while (holder.length > 1) {
+        const node = holder.shift();
         if (node === bookmark) {
-            counter.push(0);
-            arr.push(bookmark);
+            counters.push(0);
+            holder.push(bookmark);
         } else {
-            arr.push(...node.children);
-            counter[counter.length - 1]++;
-        }                
+            holder.push(...node.children);
+            counters[counters.length - 1]++;
+        }
     }
-
-    return counter;
+    return counters;
 }
 
 module.exports = levelWidth;
